3e00ee5448324a4611ea9ea56c356e4a73ee6525
Mina <minason.chem@gmail.com>
2021-04-14 22:32:28 +0900

    dir auto popup

--- src/predictortoolkits/SIConvertor.java
+++ src/predictortoolkits/SIConvertor.java
@@ -5,6 +5,7 @@ package PredictorToolkits;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -237,6 +238,18 @@ public class SIConvertor extends javax.swing.JFrame {
 
             }
             JOptionPane.showMessageDialog(null, "Done!");
+            String[] coords = txtCoordinatesPath.getText().split("\\\\");
+            String path = "";
+            for (int i = 0; i < coords.length-1; i++){
+                path = path + coords[i] + "\\";
+            }
+            File file = new File(path);
+            JOptionPane.showMessageDialog(null, path);
+            try {
+                Desktop.getDesktop().open(file);
+            } catch (IOException ex) {
+            }
+            
         }
     }//GEN-LAST:event_btnConvertActionPerformed
 
@@ -244,29 +257,6 @@ public class SIConvertor extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SIConvertor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SIConvertor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SIConvertor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SIConvertor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
